004671a3063a5c419673d0ec5de4941188307d85
Jamshidbek <jamanu1910077@gmail.com>
2023-01-01 17:55:20 +0500

    MovieInfo added and mobile layout developed

--- src/components/Layout/BaseLayout.tsx
+++ src/components/Layout/BaseLayout.tsx
@@ -1,13 +1,56 @@
-import { Navbar } from "../Navbar"
+import { useState, useEffect } from "react"
+import { DesktopNavbar, MobileNavbar } from "../Navbar"
+import { MovieInfo } from "../MovieInfo"
 import styled from "styled-components"
+import { Col } from "reactstrap"
 
-const Container = styled(`div`)``
-function App() {
-  return (
-    <Container>
-      <Navbar />
-    </Container>
+const DesktopContainer = styled(`div`)`
+  color: black;
+`
+const MobileContainer = styled("div")`
+  display: flex;
+`
+const GenresContainer = styled(Col)`
+  justify-content: center;
+  align-items: center;
+`
+const Genres = styled.div`
+  padding: 0.5rem;
+  margin: 0rem 0.5rem;
+`
+const Logo = styled.div`
+  font-family: "Lucida Console", Monaco, monospace;
+  font-size: 2.5rem;
+  margin: 0rem 1rem;
+  color: red;
+`
+
+function BaseLayout() {
+  const [width, setWindowWidth] = useState(0)
+  useEffect(() => {
+    updateDimensions()
+
+    window.addEventListener("resize", updateDimensions)
+    return () => window.removeEventListener("resize", updateDimensions)
+  }, [])
+  const updateDimensions = () => {
+    const width = window.innerWidth
+    setWindowWidth(width)
+  }
+  const Desktop = (
+    <DesktopContainer>
+      <DesktopNavbar />
+      <MovieInfo />
+    </DesktopContainer>
   )
+  const Mobile = (
+    <MobileContainer>
+      <MobileNavbar />
+      <MovieInfo />
+    </MobileContainer>
+  )
+
+  return <>{width > 570 ? Desktop : Mobile}</>
 }
 
-export default App
+export default BaseLayout
---
+++ src/components/MovieInfo/index.ts
@@ -0,0 +1,1 @@
+export { default as MovieInfo } from "./HeadMovieInfo"
