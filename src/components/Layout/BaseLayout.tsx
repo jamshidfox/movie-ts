4fd6bd2302f6fd32ca67f9a4c5ec6c53ebac30b9
Jamshidbek <jamanu1910077@gmail.com>
2023-01-01 13:17:45 +0500

    navbar added

---
+++ src/components/Layout/BaseLayout.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../Navbar"
+import styled from "styled-components"
+
+const Container = styled(`div`)``
+function App() {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  )
+}
+
+export default App
