b730aae7fa51fefda7d90e08128faf3d4cfd4296
MRayimov <mraimov1206@gmail.com>
2023-01-03 18:10:01 +0500

    responsive added

--- src/components/Layout/BaseLayout.tsx
+++ src/components/Layout/BaseLayout.tsx
@@ -1,8 +1,6 @@
-import { useState, useEffect } from "react"
 import { DesktopNavbar } from "../Navbar"
 import { MovieInfo } from "../MovieInfo"
 import styled from "styled-components"
-import { Col } from "reactstrap"
 
 const Container = styled(`div`)`
   @media (max-width: 580px) {
